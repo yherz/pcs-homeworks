4d8523b46ff1ecea7041f136252d0d6f7b672fbb
yherz <yherz18@gmail.com>
2017-06-15 21:01:25 -0400

    hw56

---
+++ 56/HW56.php
@@ -0,0 +1,44 @@
+<?php
+ echo"Donald Trump";
+ echo"2017";
+?>
+
+<?php
+    $x= "2017";
+    $y="Donald Trump";
+ echo $x;
+ echo $y;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+   <h1> <?php
+ echo"Donald Trump";
+ echo"2017";
+?></h1>
+<table class="table table-bordered">
+<tbody>
+<tr>
+<td><?=$y?></td>
+<td><?="$x-"?></td>
+</tr>
+<tr>
+<td><?="Barack Obama"?></td>
+<td><?="2009-2017"?></td>
+</tr>
+<tr>
+<td><?="George W. Bush"?></td>
+<td><?="2001-2009"?></td>
+</tr>
+</tbody>
+</table>
+</body>
+</html>
